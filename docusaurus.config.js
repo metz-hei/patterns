6a937ac25bcd53adc777e194447949bad4005bf7
Михаил Шамин <shamin.michael@gmail.com>
2025-05-20 11:44:02 +0300

    fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,6 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/metz-hei/patterns',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
